2c449b53391ba9e1ec218cf947cfaf26051c6001
JavierHenao00 <javiercamilohv00@gmail.com>
2023-04-08 00:16:54 -0500

    Cambios finales proyecto

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let count_EL= document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let resultEl = document.getElementById("result-el")
 let count=0
+let counter = 0
 
 function increment(){
     count+=1
+    counter+=1
     count_EL.innerText =count
 }
 
@@ -12,5 +15,14 @@ function save(){
 
     saveEl.textContent += str 
     count_EL.innerText =0
+    count =0
 }
 
+function result(){
+    let str = counter 
+    
+    // let amount = str + count
+    // let res =  amount
+    let text = "El total de clientes es: "
+    resultEl.textContent = text + str 
+}
